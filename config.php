018e6b8ab4f92526b805293e3eaa2fefa38200fa
Richard Dip Roy <78902181+RoyDipok@users.noreply.github.com>
2023-02-07 11:40:40 +0600

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php 
-if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
-$data['mikhmon'] = array ('1'=>'mikhmon<|<admin','mikhmon>|>aWRnbw==');
-
-$data['IT-ZONE'] = array ('1'=>'IT-ZONE!103.7.248.46','IT-ZONE@|@test','IT-ZONE#|#rJalrA==','IT-ZONE%IT-ZONE','IT-ZONE^itzone.net','IT-ZONE&tk','IT-ZONE*10','IT-ZONE(1','IT-ZONE)','IT-ZONE=10','IT-ZONE@!@enable');
-$data['new-1277'] = array ('1'=>'new-1277!','new-1277@|@','new-1277#|#','new-1277%','new-1277^','new-1277&Rp','new-1277*10','new-1277(1','new-1277)','new-1277=10','new-1277@!@disable');
